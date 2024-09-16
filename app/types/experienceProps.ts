775da2bfecec269222435b0e6d2d59ff14cf9297
lyl <johnlyl@geniihut.com>
2024-09-16 12:59:34 +0800

    WIP: change typescript exp props

--- app/types/experienceProps.ts
+++ app/types/experienceProps.ts
@@ -14,7 +14,7 @@ export const experience: ExperienceProps[] = [
     company: "Payruler",
     workLocation: "Lahug, Cebu City",
     setup: "Hybrid",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mollis, mi a laoreet scelerisque, quam quam eleifend urna, sed imperdiet leo nibh vel felis. Integer vel vulputate velit. Mauris nec efficitur erat. Fusce at placerat orci. Curabitur dignissim purus volutpat nulla imperdiet, quis mollis justo dictum. Mauris pulvinar, magna finibus maximus vehicula, velit turpis bibendum dolor, sed laoreet nunc lacus nec lacus. Nulla efficitur lacus vel elementum ultricies. In pharetra mi elit, at pellentesque eros tincidunt eu. Suspendisse mattis dui nisl, in lacinia odio fermentum et. Suspendisse sed euismod lectus. Fusce felis velit, pharetra ac vestibulum nec, malesuada et orci. Mauris non est ac tortor dapibus vehicula nec iaculis ex. Ut erat tellus, sodales vel ex vitae, pulvinar finibus justo. Fusce et lorem nec nisi condimentum dictum. Nunc semper condimentum condimentum.",
+    desc: " Developed Web Applications: Collaborated with the development team to design, implement and deploy web applications using HTML5, CSS3, JavaScript,jQuery,PostgreSQL,PHP AND Codeigniter framework.  Analyzed and optimized existing codebases to improve performance and efficiency. Assisted to normalize database design and management using PostgreSQL, ensuring data integrity and optimizing query performance. Identified and resolve bugs in the software, improving the user experience and functionality",
   },
   {
     dateOfEmployment: "Dec. 2019 - January. 2020",
@@ -22,6 +22,6 @@ export const experience: ExperienceProps[] = [
     company: "Tep J's Computer Shop",
     workLocation: "Alaska Mambaling, Cebu City",
     setup: "On Site",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mollis, mi a laoreet scelerisque, quam quam eleifend urna, sed imperdiet leo nibh vel felis. Integer vel vulputate velit. Mauris nec efficitur erat. Fusce at placerat orci. Curabitur dignissim purus volutpat nulla imperdiet, quis mollis justo dictum. Mauris pulvinar, magna finibus maximus vehicula, velit turpis bibendum dolor, sed laoreet nunc lacus nec lacus. Nulla efficitur lacus vel elementum ultricies. In pharetra mi elit, at pellentesque eros tincidunt eu. Suspendisse mattis dui nisl, in lacinia odio fermentum et. Suspendisse sed euismod lectus. Fusce felis velit, pharetra ac vestibulum nec, malesuada et orci. Mauris non est ac tortor dapibus vehicula nec iaculis ex. Ut erat tellus, sodales vel ex vitae, pulvinar finibus justo. Fusce et lorem nec nisi condimentum dictum. Nunc semper condimentum condimentum.",
+    desc: "Assist customers with inquiries about products and services. Assist in setting up and troubleshooting home or small business networks. Gain hands-on experience with hardware installation.",
   },
 ];
