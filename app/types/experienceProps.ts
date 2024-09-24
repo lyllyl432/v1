991ab305983b5fedd4fc80fa297ea9da2932edaf
lyl <johnlyl@geniihut.com>
2024-09-24 10:43:46 +0800

    WIP: change intro

--- app/types/experienceProps.ts
+++ app/types/experienceProps.ts
@@ -16,12 +16,4 @@ export const experience: ExperienceProps[] = [
     setup: "Hybrid",
     desc: " Developed Web Applications: Collaborated with the development team to design, implement and deploy web applications using HTML5, CSS3, JavaScript,jQuery,PostgreSQL,PHP AND Codeigniter framework.  Analyzed and optimized existing codebases to improve performance and efficiency. Assisted to normalize database design and management using PostgreSQL, ensuring data integrity and optimizing query performance. Identified and resolve bugs in the software, improving the user experience and functionality",
   },
-  {
-    dateOfEmployment: "Dec. 2019 - January. 2020",
-    position: "Computer Technician Intern",
-    company: "Tep J's Computer Shop",
-    workLocation: "Alaska Mambaling, Cebu City",
-    setup: "On Site",
-    desc: "Assist customers with inquiries about products and services. Assist in setting up and troubleshooting home or small business networks. Gain hands-on experience with hardware installation.",
-  },
 ];
